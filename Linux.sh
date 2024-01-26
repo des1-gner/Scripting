2bf4d79b6a3a0ee2d3001e8c676e3eea8c16a213
Oisin Aeonn <99879353+des1-gner@users.noreply.github.com>
2024-01-26 17:41:31 +1100

    Add files via upload

---
+++ Linux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Oisin Aeonn Scripting 2 s3952320
+
+# had a menu, but did not work with crontab...
+
+# overwrite linux.log 
+echo "Updating" 
+echo " " | sudo -S sudo apt-get update > /home/des1gner/Desktop/Linux.log
+# add to linux.log
+echo "Upgrading" 
+echo " " | sudo -S sudo apt-get -y upgrade >> /home/des1gner/Desktop/Linux.log
+echo "Update Successful, Please Check Log. "
+
+# had a bug - requires full length directory for crontab to function and create file...
